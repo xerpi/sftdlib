a7810e33dfb1eb218c4c5fbd67fa80f1cb28b676
Sergi Granell <xerpi.g.12@gmail.com>
2015-04-02 22:55:44 +0200

    First commit

---
+++ libsftd/include/sftd.h
@@ -0,0 +1,31 @@
+/**
+ * @file sftd.h
+ * @author Sergi Granell (xerpi)
+ * @date 2 April 2015
+ * @brief sftdlib header
+ */
+
+#ifndef SFTD_H
+#define SFTD_H
+
+#include <3ds.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Defines
+
+// Enums
+
+// Structs
+
+// Basic functions
+
+// Draw functions
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ libsftd/source/sftd.c
@@ -0,0 +1,1 @@
+#include "sftd.h"
